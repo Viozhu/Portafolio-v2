006ec32b7d6387e7bad37eaa7fadd08950f0171e
Jorge Ignacio Garay <jorgeignaciogaray@MacBook-Air-de-Jorge.local>
2022-01-26 12:04:44 -0300

    fix

--- components/Contact/contact.tsx
+++ components/Contact/contact.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Contact = () => {
     return (
-        <div className='flex  md:w-[40rem]  sm:w-auto mx-auto mt-12 pb-24 justify-around'>
+        <div className='flex w-auto sm:w-[40rem] mx-auto mt-12 pb-24 justify-around'>
             <a href='https://www.linkedin.com/in/jorgeignaciogaray/'><img src="https://github.com/Viozhu/Viozhu.github.io/blob/main/Iconos/linkedin.png?raw=true" alt="link" width="60" height="60" /></a>
             <a href='https://mail.google.com/a/?view=cm&fs=1&to=garayjorgeignacio@gmail.com'><img src="https://github.com/Viozhu/Viozhu.github.io/blob/main/Iconos/gmail.png?raw=true" alt="gmail" width="60" height="60" /></a>
             <a href='https://wa.me/5493434592663?text=Hi!%20I%20saw%20your%20portafolio'><img src="https://github.com/Viozhu/Viozhu.github.io/blob/main/Iconos/whatsapp.png?raw=true" alt="whtsapp" width="60" height="60" /></a>
